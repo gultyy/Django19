49416fc7d38aa4c571857fc5fa352b5ba067e72f
Vlad <vadyapb8@gmail.com>
2024-11-10 16:46:20 +0300

    Initial commit

---
+++ django19/task1/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Buyer(models.Model):
+    name=models.CharField(max_length=100)
+    balance=models.DecimalField(decimal_places=3, max_digits=10)
+    age=models.IntegerField()
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    cost = models.DecimalField(decimal_places=3, max_digits=10)
+    size = models.DecimalField(decimal_places=3, max_digits=10)
+    description = models.TextField()
+    age_limited = models.BooleanField(default=False)
+    buyer = models.ManyToManyField(Buyer, related_name='games')
